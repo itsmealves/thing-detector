b1e1efb13c523b2c9f143ca15c6b95be534d0b5e
Gabriel Alves <itsmealves@gmail.com>
2018-07-02 02:12:34 -0300

    working version

--- web/views.py
+++ web/views.py
@@ -2,13 +2,7 @@
 from __future__ import unicode_literals
 
 import uuid
-import keras
-import numpy as np
-from PIL import Image
-from keras.applications import resnet50
-from keras.preprocessing.image import load_img
-from keras.preprocessing.image import img_to_array
-from keras.applications.imagenet_utils import decode_predictions
+import requests
 from django.core.files.storage import default_storage
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render, redirect
@@ -23,20 +17,6 @@ def not_found(request):
     return redirect('/')
 
 class FileView(View):
-    def run_detection(self, file):
-        image = Image.open(file.file).resize((224, 224))
-
-        resnet_model = resnet50.ResNet50(weights='imagenet')
-        matrix = img_to_array(image)
-        image_batch = np.expand_dims(matrix, axis=0)
-        processed_image = resnet50.preprocess_input(image_batch.copy())
-        predictions = resnet_model.predict(processed_image)
-
-        print(decode_predictions(predictions))
-
-        return file
-
-
     def get(self, request):
         key = ''
         url = ''
@@ -56,19 +36,21 @@ class FileView(View):
 
 
     def post(self, request):
-        file = self.run_detection(request.FILES['file'])
+        file = request.FILES['file']
+
+        print(type(file))
+
         key = uuid.uuid4().hex[0:20]
-        file_name = str(uuid.uuid4()) + '.' + file.name.split('.')[-1]
+        extension = file.name.split('.')[-1]
+        file_name = str(uuid.uuid4()) + '.' + extension
 
+        files = {'file': file}
+        payload = {'filename': file_name}
+        response = requests.post('http://localhost:4000', data=payload, files=files).json()
+        url = response['url']
 
-        with default_storage.open(file_name, 'w') as destination:
-            for chunk in file.chunks():
-                destination.write(chunk)
-
-        url = default_storage.url(file_name).split('?')[0]
-
-        new_detection = Detection(key=key, url=url)
-        new_detection.save()
+        detection = Detection(key=key, url=url)
+        detection.save()
 
         return JsonResponse({
             'url' : url,
